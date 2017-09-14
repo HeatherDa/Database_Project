3febf8c65e908b05c352e14910e28650e096af6a
HeatherDa <crystal_tree@hotmail.com>
2017-09-14 16:08:37 -0500

    Working a little bit, but doesn't seem to do anything?

--- Main.py
+++ Main.py
@@ -9,24 +9,51 @@ db=False
 
 filename= 'p2_db.sqlite'  # name of the sqlite database file
 tableName = 'products'  # name of the table to be created
-columns = ['itemNum','itemType', 'itemDescription', 'itemVendor', 'itemExp', 'itemValue', 'itemQuantity']
-types = ['INTEGER','TEXT', 'TEXT', 'INTEGER', 'TEXT', 'TEXT', 'INTEGER']
+
 #        # Connecting to the database file
 def useDatabase(option):
+    print("use database")
+    global filename
+    global tableName
+    columns = ['itemNum', 'itemType', 'itemDescription', 'itemVendor', 'itemExp', 'itemValue', 'itemQuantity']
+    types = ['INTEGER', 'TEXT', 'TEXT', 'INTEGER', 'TEXT', 'TEXT', 'INTEGER']
+    like = "LIKE '[0-9][0-9][0-9][0-9][0-9]'"
     try:
         connect = sqlite3.connect(filename)
         c=connect.cursor()
-
         if option==1:
-            makeTable(c)
+            c.execute('CREATE TABLE {tn}({col}{field} PRIMARY KEY)'.format(tn=tableName, col=columns[0], field=types[0]))
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemType' TEXT")
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemDescription' TEXT")
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemVendor' INTEGER")#can't get this to work CHECK (itemVendor {c}".format (c=like))
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemCost' TEXT")
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemValue' TEXT")
+            c.execute("ALTER TABLE Products ADD COLUMN 'itemQuantity' INTEGER DEFAULT 0")
+
         elif option==2:
-            newRow(c)
+            se = input("Please type in the values for the new row separated by commas.")
+            v = se.split(',')
+            #v.insert(0,'NULL')
+            var = []
+            for value in v:
+                a = value.strip()
+                var.append(a)
+            val = ('NULL', var[0], var[1], var[2], var[3], var[4], var[5])
+            #sql = '''INSERT INTO products(itemType,itemDescription,itemVendor,itemCost,itemValue,itemQuantity) VALUES(NULL,?,?,?,?,?,?)'''
+            sql = '''INSERT INTO products VALUES(?,?,?,?,?,?,?)'''
+            c.execute(sql, val)
+            print("data entered was ", val)
         elif option==3:
-            updateRow(c)
+            upIn=input("Please type in the item number and the new quantity separated by a comma.")
+            inf=upIn.split(',')
+            #This will throw an error if the item id entered does not exist
+            c.execute("UPDATE {tn} SET {col}='{inp}'WHERE {itemNum}={id}".format(tn=tableName,col='itemQuantity',inp=inf[0],id=inf[1]))
         elif option==4:
-            deleteRow(c)
+            deleteRow(c,option)
         elif option==5:
-            displayAll(c)
+            c.execute("SELECT * FROM {tn}".format(tn=tableName))
+            record=c.fetchone()
+            print('{}'.format(record))
         elif option==6:
             s = input("What is the number of the item you are looking for?")
             if isInt(s) and len(s) == 5:
@@ -34,6 +61,7 @@ def useDatabase(option):
         else:
             print("That is not one of the options on the menu.")
         db=True
+
 
     except Error as e:
         print(e)
@@ -44,36 +72,56 @@ def useDatabase(option):
 
 connect=sqlite3.connect(filename)
 c = connect.cursor()
-checkString = "LIKE '[0-9][0-9][0-9][0-9][0-9]'"
+
 
 
 
 
 #Lets make a table in the database
-def makeTable(c):
+"""def makeColumns(i, type):
+    global tableName
+    like = "LIKE '[0-9][0-9][0-9][0-9][0-9]'"
+    print("makeTable")
+    #for i in columns:
+    if (i != 'itemVendor') and (i != 'itemQuantity') and (i != 'itemNum'):
+        r=str("ALTER TABLE {tn} ADD COLUMN'{nf}''{ft}'".format(tn=tableName,nf=i, ft=type))
+        print(r)
+        return r
 
-    for i in columns:
-        if (i != 'itemVendor') and (i != 'itemQuantity') and (i != 'itemNum'):
-            c.execute("ALTER TABLE {tn} ADD COLUMN'{nf}''{ft}'".\
-                format(tn=tableName,nf=i, ft=types.index(i)))
+    elif i == 'itemQuantity': #uses default value of 0 for Quantitiy
+        r=str("ALTER TABLE {tn} ADD COLUMN'{nf}'{ft} DEFAULT '{d}'".format(tn=tableName,nf=i, ft=type, d=0))
+            #format(tn=tableName,nf=i, ft=types.index(i), d=0))
+        print(r)
+        return r
 
-        elif i == 'itemQuantity': #uses default value of 0 for Quantitiy
-            c.execute("ALTER TABLE {tn} ADD COLUMN'{nf}'{ft} DEFAULT '{d}'".\
-                format(tn=tableName,nf=i, ft=types.index(i), d=0))
+    elif i=='itemVendor':   #uses CHECK to ensure that vendor numbers are 5 digit numbers
+      # Will eventually point to a Vendor's table, but can't set that up without having a Vendor's table
+        r=str("ALTER TABLE {tn} ADD COLUMN'{nf}'{ft} CHECK (itemVendor {c})".format(tn=tableName,nf=i, ft=type, c=like))
+            #format(tn=tableName, nf=i, ft=types.index(i), d=0, c=checkString))
+        print (r)
+        return r
 
-        elif i=='itemVendor':   #uses CHECK to ensure that vendor numbers are 5 digit numbers
-         # Will eventually point to a Vendor's table, but can't set that up without having a Vendor's table
-            c.execute("ALTER TABLE {tn} ADD COLUMN'{nf}'{ft} CHECK (itemVendor {c})".\
-                format(tn=tableName, nf=i, ft=types.index(i), d=0, c=checkString))
+#    else: #it's the primary key, used CHECK to ensure that itemNums are 5 digit numbers
+#        r=("ALTER TABLE {tn} ADD COLUMN'itemNum' {ft} PRIMARY KEY CHECK (itemNum {c})".
+#            format(tn=tableName, ft=types[0], c=checkString))
+#        print(r)
+#        return r
+"""
 
-        else: #it's the primary key, used CHECK to ensure that itemNums are 5 digit numbers
-            c.execute("ALTER TABLE {tn} ADD COLUMN'{nf}' {ft} PRIMARY KEY, CHECK (itemNum {c})". \
-                format(tn=tableName, nf=columns[0], ft=types[0], c=checkString))
 
 def newRow(c):
 #Something to add a row.
+    se=input("Please type in the values for the new row separated by commas.")
+    v=se.split(',')
+    var=[]
+    for value in v:
+        a=value.strip()
+        var.append(a)
+    val=(var[0],var[1],var[2],var[3],var[4],var[5],var[6])
+
     print("Not done writing addRow yet, here's the data so far ")
-#self.connect.commit()
+    sql='''INSERT INTO products(itemNum,itemType,itemDescription,itemVendor,itemCost,itemValue,itemQuantity) VALUES(?,?,?,?,?,?,?)'''
+    c.execute(sql,val)
 
 def updateRow(c):
     print("still working on update, but I got. ")
@@ -101,42 +149,13 @@ def deleteRow (c, option):
     s=input("What is the item number of the item you wish to delete?")
     search(c, s,option)
 
-
-
-
-
-
-
-import sqlite3
-#import Database
-
-#daba=Database.Database() # defined or initialized?  Want defined, not initialized....
-
-def dbExists(option):
-    global db
-    if not db: # don't know if this will work to tell me if it exists, ie has been initialized.'
-        print('no daba, so make one')
-        if option !=1:
-            useDatabase(option)
-        return False
-    else:
-        return True
-
 def isInt (data):
     try:
         i=int(data)
         return True
-    except TypeError:
+    except ValueError:
         print ("The data you have entered is not an integer.")
         return False
-
-#def isText(data):
-#    try:
-#        s=str(data)
-#        return True
-#    except TypeError:
-#        print ("This field needs a string.") #when would you ever use this?  Data always comes in as a string...
-stTest="I'm a test"
 
 def main():
     global daba
@@ -144,54 +163,15 @@ def main():
     global stTest
 #    import Database
     print("This program helps you to create and use a products database.  \nPlease choose one of the following menu options.")
-    option=int(input("1: Create a Database and Table\n2: Add a row to the table\n3: Update a row in the table\n4: Delete a row in the table\n5: Display all rows in the table\n6: Search for a specific row and display it."))
-    if(isInt(option)): #test for numbers
-        if option==1:
-            dbExists(option)
-        elif option==2:
-            if dbExists(option):
-                newRow(daba, stTest)
-            else:
-                print("Please create the Database before attempting to use it.")
-        elif option==3:
-            if dbExists(option):
-                updateRow(daba,stTest)
-            else:
-                print("Please create the Database before attempting to use it.")
-        elif option==4:
-            if dbExists(option):
-                deleteRow(daba, option)
-            else:
-                print("Please create the Database before attempting to use it.")
-        elif option==5:
-            if dbExists(option):
-                displayAll(daba)
-            else:
-                print("Please create the Database before attempting to use it.")
-        elif option==6:
-            if dbExists(option):
-                searchPar=input("Please type the item number of the record you are looking for.")
-                if (isInt(searchPar) and (len(searchPar)==5)): #right now, search only looks for item numbers.  Improve later.
-                    search(daba, searchPar, option)
-                else:
-                    print("That is not a valid item number.  An item number has 5 numeric digits.")
-            else:
-                print("Please create the Database before attempting to use it.")
-        else:
-            print("That is not one of the options on the menu.  To select an item from the menu, type its number.")
-
-
-
-
-
-
-
-
-
-
+    option=input("1: Create a Database and Table\n2: Add a row to the table\n3: Update a row in the table\n4: Delete a row in the table\n5: Display all rows in the table\n6: Search for a specific row and display it.")
+    if isInt(option):
+        op=int(option)
+        if (op >0) & (op <7): #test for numbers
+            useDatabase(op)
+    else:
+        print("That is not one of the options on the menu.  To select an item from the menu, type its number.")
 
 main()
-#close connection.
 
 
 
